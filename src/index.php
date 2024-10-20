b1fd5c375088f7a92591e3e974c9a396145e61e9
Michal Konik <---->
2024-10-20 22:44:18 +0200

    #rc - finished task with unit tests

--- src/index.php
+++ src/index.php
@@ -8,8 +8,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 header('Content-Type: application/json');
 
-$request = ['term' => htmlspecialchars($_GET['term']), 'amount' => htmlspecialchars($_GET['amount'])];
-$feeStructureLoader = new FeeStructureLoader(__DIR__ . '/Resource/FeeConfig/feeStructure.json');
-$feeCalculator = new FeeCalculator($feeStructureLoader);
-$fee = $feeCalculator->calculate(new LoanProposal($request['term'], $request['amount']));
-echo json_encode(['fee' => $fee]);
+try {
+    $request = ['term' => htmlspecialchars($_GET['term']), 'amount' => htmlspecialchars($_GET['amount'])];
+    $feeStructureLoader = new FeeStructureLoader(__DIR__ . '/Resource/FeeConfig/feeStructure.json');
+    $feeCalculator = new FeeCalculator($feeStructureLoader);
+    $fee = $feeCalculator->calculate(new LoanProposal($request['term'], $request['amount']));
+    echo json_encode(['fee' => $fee]);
+} catch (Exception $exception) {
+    echo json_encode(['error' => $exception->getMessage()]);
+}
